76700ed0f99a0fde94381328e2256024f4596b0b
ralucaioanaianos <raluca_ianos@yahoo.com>
2024-01-15 19:10:18 +0200

    added img

--- Tomcat/QuizApp/src/main/java/SecondServlet.java
+++ Tomcat/QuizApp/src/main/java/SecondServlet.java
@@ -20,11 +20,23 @@ public class SecondServlet extends HttpServlet {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<html>");
-        out.println("<head><title>Quiz Result</title></head>");
+        out.println("<head><title>Quiz Result</title>");
+        out.println("<style>");
+        out.println("body {");
+        out.println("    font-family: 'Arial', sans-serif;");
+        out.println("    display: flex;");
+        out.println("    align-items: center;");
+        out.println("    justify-content: center;");
+        out.println("    height: 100vh;");
+        out.println("    margin: 0;");
+        out.println("}");
+        out.println("img { width: 100px; height: 100px; }");
+        out.println("h1 { text-align: center; }");
+        out.println("</style>");
+        out.println("</head>");
         out.println("<body>");
-        out.println("<h3>Felicitari! Ai obtinut " + totalPoints + " puncte!</h3>");
-        out.println("</body>");
-        out.println("</html>");
+        out.println("<img src='confetti.gif' alt='Confetti' />");
+        out.println("<h1>Felicitari! Ai obtinut " + totalPoints + " puncte!</h1>");
 
         // Close PrintWriter
         out.close();
